ee7bf1aafeb7e47adfee4b83586078e42216b03b
mironshoxsayfullaev2010-a1ly <mironshoxsayfullaev2010@gmail.com>
2026-01-15 15:34:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Coin from "./pages/Home/Coin/Coin";
+import Footer from "./components/footer/footer";
+import Features from "./pages/Features/Features";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin/:coinId" element={<Coin />} />
+        <Route path="/features" element={<Features />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Features/Features.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Features() {
+  return (
+    <div className="container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, labore
+      aut accusamus possimus autem, delectus provident rerum similique
+      consectetur dolore, harum mollitia? Quas, assumenda ea labore aperiam esse
+      odio laboriosam?
+    </div>
+  );
+}
+
+export default Features;
